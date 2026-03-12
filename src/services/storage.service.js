17b63d732e93cb115b6ab76dfdfe9926cd9a5cd2
Nilraj <nilrajj0100@gmail.com>
2026-03-12 11:38:20 +0530

    Fix ImageKit variable case bug

--- src/services/storage.service.js
+++ src/services/storage.service.js
@@ -1,20 +1,20 @@
 const ImageKit = require("@imagekit/nodejs");
 
 const imagekit = new ImageKit({
-  publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 async function uploadFile(file, fileName) {
-  return imageKit.upload({
+  return imagekit.upload({
     file,
     fileName,
   });
 }
 
 async function deleteFile(fileId) {
-  return imageKit.deleteFile(fileId);
+  return imagekit.deleteFile(fileId);
 }
 
 module.exports = {
